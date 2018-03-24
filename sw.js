beb16887b89830a495338f992f9f5749bf59f006
Frits <frits.vanturnhout@gmail.com>
2018-03-24 16:03:49 +0100

    changes

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const VERSION = 'v1';
+const VERSION = 'v2';
 const RUNTIME = 'sw';
 
 const PRECACHE_URLS = [
   'css/styles.css',
   'js/main.js',
-  'data/restaurants.json'
+  'js/dbhelper.js',
+  'js/restaurant_info.js',
+  'data/restaurants.json',
+  'index.html',
+  'restaurant.html',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',	
 ];
 
 // The install handler takes care of precaching the resources we always need.
